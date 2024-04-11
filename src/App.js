cff5e3cb5ceb0d3ae0556b72da15925cd268ab6a
sansritisharma <87527606+sansritisharma@users.noreply.github.com>
2024-04-11 17:40:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let date = new Date("2024-04-11 00:00"); // Year, Month (0-based), Day, Hour
+  let h = date.getHours(); // 0-23
+  let greeting = "";
+  let mystyle = {};
+
+  if (h >= 0 && h < 12) {
+    greeting = "Morning";
+    mystyle.color = "white";
+  } else if (h >= 12 && h < 16) {
+    greeting = "Afternoon";
+    mystyle.color = "yellow";
+  } else if (h >= 16 && h < 20) {
+    greeting = "Evening";
+    mystyle.color = "orange";
+  } else if (h >= 20) {
+    greeting = "Night";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hi sir, <span style={mystyle}>Good {greeting}</span>
+      </h1>
     </div>
   );
 }
